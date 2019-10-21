8f7b27a676a52557563ef179943eea749b1d7065
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-10-21 13:03:17 +0600

    Update ContactController.php

--- Contact/ContactController.php
+++ Contact/ContactController.php
@@ -1,19 +1,23 @@
-use Illuminate\Support\Mail;
-
-public fuction create()
+<?php
+namespace App\Http\Controllers;
+use App\Mail\ContactFormMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+class ContactFormController extends Controller
 {
-  return view('contact.create');
-}
-public function store()
-{
-  $data = request()->valudate([
-    'name' => 'required',
-    'email' => 'required|email',
-    'message' => 'required',
-  ]);
-  
-  Mail::to('test@test.com')->send(new ContactMail($data));
-  
-  Return redirect('contact')->with('message', 'Thanks for your message, We will be in touch.');
-  
+    public function create()
+    {
+        return view('contact.create');
+    }
+    public function store()
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+        Mail::to('test@test.com')->send(new ContactFormMail($data));
+        return redirect('contact')
+            ->with('message', 'Thanks for your message. We\'ll be in touch.');
+    }
 }
