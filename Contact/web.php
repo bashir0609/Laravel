28252e6f5835b429c9bd8d3509493a90ef44c6c3
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-10-21 13:00:53 +0600

    Update web.php

--- Contact/web.php
+++ Contact/web.php
@@ -1,2 +1,15 @@
-Route::get('contact', 'ContactController@create');
-Route::get('contact', 'ContactController@store');
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('contact', 'ContactFormController@create')->name('contact.create');
+Route::post('contact', 'ContactFormController@store')->name('contact.store');
+
