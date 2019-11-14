ea6711213752fe944fa40afa058f01094ae0a9b0
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 12:59:26 +0600

    Update form.blade.php

--- Functions/Eloquent Where Clause/form.blade.php
+++ Functions/Eloquent Where Clause/form.blade.php
@@ -1,10 +1,15 @@
+```
 <div class="form-group">
     <label for="active">Status</label>
     <select name="active" id="active" class="form-control">
         <option value="" disabled>Select customer status</option>
 
         @foreach($customer->activeOptions() as $activeOptionKey => $activeOptionValue)
-            <option value="{{ $activeOptionKey }}" {{ $customer->active == $activeOptionValue ? 'selected' : '' }}>{{ $activeOptionValue }}</option>
+            <option value="{{ $activeOptionKey }}" {{ $customer->active == $activeOptionValue ? 'selected' : '' }}>
+                {{ $activeOptionValue }}
+            </option>
         @endforeach
     </select>
 </div>
+
+```
