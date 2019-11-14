eed4c2c166029ef93dd858134c6945f540ae45dc
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 13:38:27 +0600

    Update CustomersController.php

--- Functions/Eloquent Where Clause/CustomersController.php
+++ Functions/Eloquent Where Clause/CustomersController.php
@@ -1,4 +1,11 @@
 ```
+    public function index()
+    {
+        $activeCustomers = Customer::where('active', 1)->get();
+        $inactiveCustomers = Customer::where('active', 0)->get();
+
+        return view('customers.index', compact($activeCustomers, $inactiveCustomers));
+    }
     public function store()
     {
         $data = request()->validate([
