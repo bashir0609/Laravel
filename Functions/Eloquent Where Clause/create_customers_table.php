9fe3352951a76632592beebb14a9642f85d550fc
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 13:06:43 +0600

    Update create_customers_table.php

--- Functions/Eloquent Where Clause/create_customers_table.php
+++ Functions/Eloquent Where Clause/create_customers_table.php
@@ -3,11 +3,9 @@
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('company_id');
             $table->string('name');
             $table->string('email');
             $table->integer('active');
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
