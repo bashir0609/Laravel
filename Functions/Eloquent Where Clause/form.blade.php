87c8cb64a6deb692cc074e3a88f418e73d676d53
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 12:56:37 +0600

    Create form.blade.php

---
+++ Functions/Eloquent Where Clause/form.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+    <label for="active">Status</label>
+    <select name="active" id="active" class="form-control">
+        <option value="" disabled>Select customer status</option>
+
+        @foreach($customer->activeOptions() as $activeOptionKey => $activeOptionValue)
+            <option value="{{ $activeOptionKey }}" {{ $customer->active == $activeOptionValue ? 'selected' : '' }}>{{ $activeOptionValue }}</option>
+        @endforeach
+    </select>
+</div>
