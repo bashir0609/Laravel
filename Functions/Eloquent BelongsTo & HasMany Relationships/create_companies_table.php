9413e79c194408339fc20279a704794968726ecb
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 14:05:06 +0600

    Create create_companies_table.php

---
+++ Functions/Eloquent BelongsTo & HasMany Relationships/create_companies_table.php
@@ -0,0 +1,11 @@
+```
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+```
