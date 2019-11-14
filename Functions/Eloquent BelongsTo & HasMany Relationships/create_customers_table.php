772a13358f54cb7418c1a4295f48c4de8683d9b3
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 14:14:35 +0600

    Create create_customers_table.php

---
+++ Functions/Eloquent BelongsTo & HasMany Relationships/create_customers_table.php
@@ -0,0 +1,11 @@
+public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('company_id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('active');
+            $table->timestamps();
+        });
+    }
