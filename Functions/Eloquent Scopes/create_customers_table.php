9cccfb271c44401f1f51220a45b379106684df90
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 14:46:20 +0600

    Create create_customers_table.php

---
+++ Functions/Eloquent Scopes/create_customers_table.php
@@ -0,0 +1,10 @@
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('active');
+            $table->timestamps();
+        });
+    }
