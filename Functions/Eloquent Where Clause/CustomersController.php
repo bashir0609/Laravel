4d7e10eadeb478a83c16bfdb728460c45c14945e
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-14 13:14:16 +0600

    Update CustomersController.php

--- Functions/Eloquent Where Clause/CustomersController.php
+++ Functions/Eloquent Where Clause/CustomersController.php
@@ -1,16 +1,19 @@
 ```
     public function store()
     {
-        $customer = Customer::create($this->validateRequest());
-        return redirect('customers');
-    }
-    
-    private function validateRequest()
-    {
-        return request()->validate([
+        $data = request()->validate([
             'name' => 'required|min:3',
             'email' => 'required|email',
             'active' => 'required',
         ]);
+
+        $customer = new Customer();
+        $customer->name => request('name'),
+        $customer ->email => request('email'),
+        $customer->active => requet('active'),
+        $customer->save();
+
+        return redirect('customers');
     }
+    
 ```
