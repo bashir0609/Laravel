c6c356447aee01e752dfd669f34e811ac25fd328
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-11-15 00:54:26 +0600

    Update Customer.php

--- Functions/Eloquent BelongsTo & HasMany Relationships/Customer.php
+++ Functions/Eloquent BelongsTo & HasMany Relationships/Customer.php
@@ -2,6 +2,14 @@
 class Customer extends Model
 {
   protected $guarded = [];
+  public function scopeActive($query)
+  {
+      return $query->where('active', 1);
+  }
+  public function scopeInactive($query)
+  {
+      return $query->where('active', 0);
+  }
   
   public function company()
   {
