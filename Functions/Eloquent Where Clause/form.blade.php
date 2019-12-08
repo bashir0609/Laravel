077940fe39467229b19c36a0c1233fe8adcb041c
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-12-08 17:42:53 +0600

    Delete form.blade.php

--- Functions/Eloquent Where Clause/form.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-    <label for="active">Status</label>
-    <select name="active" id="active" class="form-control">
-        <option value="" disabled>Select customer status</option>
-        <option value="1">Active</option>
-        <option value="0">Active</option>
-    </select>
-</div>
