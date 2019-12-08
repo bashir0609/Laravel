c4781fcf557c5c233dc70faceac71305e1f9c487
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-12-08 17:41:23 +0600

    Delete form.blade.php

--- Functions/Eloquent BelongsTo & HasMany Relationships/form.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-    <label for="company_id">Company</label>
-    <select name="company_id" id="company_id" class="form-control">
-        @foreach($companies as $company)
-          <option value="{{ $company->id }}">{{ $company->name }}</option>
-        @endforeach
-    </select>
-</div>
